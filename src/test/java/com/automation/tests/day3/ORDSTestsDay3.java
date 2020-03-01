5401e9bc731642b826d9d9636aa0604684b5e199
Tuerhobng Zhumake <ztuerhon@masonlive.gmu.edu>
2020-03-01 13:34:09 -0500

    created ORDSTestsDay3 class

--- src/test/java/com/automation/tests/day3/ORDSTestsDay3.java
+++ src/test/java/com/automation/tests/day3/ORDSTestsDay3.java
@@ -2,10 +2,18 @@ package com.automation.tests.day3;
 
 import com.automation.utilities.ConfigurationReader;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.BeforeAll;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,9 +22,37 @@ import static org.hamcrest.Matchers.*;
 public class ORDSTestsDay3 {
 
     @BeforeAll
-    public  void setup(){
-   baseURI = ConfigurationReader.getProperty("ords.uri");
-
+    public static void setup() {
+        baseURI = ConfigurationReader.getProperty("ords.uri");
     }
 
+    //accept("application/json") shortcut for header("Accept", "application/json")
+    //we are asking for json as a response
+    @Test
+    public void test1() {
+        given().
+                accept("application/json").
+                get("/employees").
+                then().
+                assertThat().statusCode(200).
+                and().assertThat().contentType("application/json").
+                log().all(true);
+    }
+
+    //path parameter - to point on specific resource /employee/:id/ - id it's a path parameter
+    //query parameter - to filter results, or describe new resource :
+    // POST /users?name=James&age=60&job-title=SDET
+    //or to filter GET /employee?name=Jamal get all employees with name Jamal
+
+
+    @Test
+    public void test2(){
+        given().
+                accept("application/json").
+                pathParam("id",100).
+                when().get("/employees/{id}").
+                then().assertThat().statusCode(200).
+                and().assertThat().body("employee_id",is(100)).
+                log().all(true);
+    }
 }
