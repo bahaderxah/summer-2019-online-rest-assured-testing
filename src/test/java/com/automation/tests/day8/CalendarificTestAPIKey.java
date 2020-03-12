cae2bf52565b77c30512f87cb094faf391cfa78c
Tuerhobng Zhumake <ztuerhon@masonlive.gmu.edu>
2020-03-12 16:39:10 -0400

    day8 CalendarificTestAPIKey

--- src/test/java/com/automation/tests/day8/CalendarificTestAPIKey.java
+++ src/test/java/com/automation/tests/day8/CalendarificTestAPIKey.java
@@ -33,17 +33,17 @@ public class CalendarificTestAPIKey {
     }
 
 
-/**
- * API key is a secret that the API generates and gives to the developer.
- * API key looks like long string: ebe88078e981c84bedeb9e8a34ad927560e545f2
- * API key can go as query parameter or inside a header,
- * it depends on web service how you must pass API key (as header parameter or query parameter)
- * How it get's created? You go to web site, register, and service gives you API key
- * Then you have to pass API key alongside with every request
- * API key is easy to implement for developer and client
- * But, non-technical people have no idea about this
- * So it's mostly used by developers only
- */
+    /**
+     * API key is a secret that the API generates and gives to the developer.
+     * API key looks like long string: ebe88078e981c84bedeb9e8a34ad927560e545f2
+     * API key can go as query parameter or inside a header,
+     * it depends on web service how you must pass API key (as header parameter or query parameter)
+     * How it get's created? You go to web site, register, and service gives you API key
+     * Then you have to pass API key alongside with every request
+     * API key is easy to implement for developer and client
+     * But, non-technical people have no idea about this
+     * So it's mostly used by developers only
+     */
 
     /**
      * Given accept content type as JSON
@@ -54,10 +54,10 @@ public class CalendarificTestAPIKey {
      */
 
 
-//status line: HTTP/1.1 401 Unauthorized
+    //status line: HTTP/1.1 401 Unauthorized
     @Test
     @DisplayName("Verify that user cannot access web service without valid API key")
-    public void test1() {
+    public void test1(){
         given().
                 accept(ContentType.JSON).
                 when().
@@ -67,12 +67,60 @@ public class CalendarificTestAPIKey {
                 statusLine(containsString("Unauthorized")).
                 body("meta.error_detail", containsString("Missing or invalid api credentials."));
     }
-}
+
+    /**
+     * Given accept content type as JSON
+     * And query parameter api_key with valid API key
+     * When user sends GET request to "/countries"
+     * Then user verifies that status code is 200
+     * And user verifies that status line contains "OK" message
+     * And user verifies that countries array is not empty
+     */
+
+    @Test
+    public void test2(){
+        given().
+                accept(ContentType.JSON).
+                queryParam("api_key", "ebe88078e981c84bedeb9e8a34ad927560e545f2").
+                when().
+                get("/countries").prettyPeek().
+                then().assertThat().statusCode(200).
+                statusLine(containsString("OK")).
+                body("response.countries", not(empty()));
+
+    }
+
+
 /**
+ *
  * Given accept content type as JSON
  * And query parameter api_key with valid API key
- * When user sends GET request to "/countries"
- * Then user verifies that status code is 200
- * And user verifies that status line contains "OK" message
- * And user verifies that countries array is not empty
+ * And query parameter country is equals to US
+ * And query parameter type is equals to national
+ * And query parameter year is equals to 2019
+ * When user sends GET request to "/holidays"
+ * Then user verifies that number of national holidays in US is equals to 11
  */
+
+@Test
+@DisplayName("Verify that there are 11 national holidays in the US")
+public void test3(){
+    Response response = given().
+            accept(ContentType.JSON).
+            queryParam("api_key","ebe88078e981c84bedeb9e8a34ad927560e545f2").
+            queryParam("country", "US").
+            queryParam("type", "national").
+            queryParam("year", 2019).
+            when().
+            get("/holidays").prettyPeek();
+    //shorter way with syntax sugar (hamcrest matcher)
+    response.then().assertThat().statusCode(200).body("response.holidays", hasSize(11));
+    //List<Map<String, ?>> - list of objects, since there are nested objects, we cannot specify some value type
+    List<Map<String, ?>> holidays = response.jsonPath().get("response.holidays");
+
+    //regular way
+    assertEquals(11, holidays.size(), "Wrong number of holidays!");
+    assertEquals(200, response.getStatusCode());
+
+}
+}
