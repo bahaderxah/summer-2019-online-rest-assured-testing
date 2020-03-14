1680075908e2710c5d75381387795c4c7474bf74
Tuerhobng Zhumake <ztuerhon@masonlive.gmu.edu>
2020-03-14 10:43:57 -0400

    day9 WarmUP

--- src/test/java/com/automation/tests/day9/WarmUP.java
+++ src/test/java/com/automation/tests/day9/WarmUP.java
@@ -27,67 +27,67 @@ import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.Matchers.*;
 
-public class WarmUP {
+        public class WarmUP {
 
 
-    /**
-     *  ####### WARM-UP TASK ########
-     * Given accept content type as JSON
-     * And query parameter api_key with valid API key
-     * When user sends GET request to "/countries"
-     * Then user verifies that total number of holidays in United Kingdom is equals to 95
-     *
-     * website: https://calendarific.com/
-     *
-     *
-     *
-     */
+            /**
+             *  ####### WARM-UP TASK ########
+             * Given accept content type as JSON
+             * And query parameter api_key with valid API key
+             * When user sends GET request to "/countries"
+             * Then user verifies that total number of holidays in United Kingdom is equals to 95
+             *
+             * website: https://calendarific.com/
+             *
+             *
+             *
+             */
 
-    @BeforeAll
-    public static void setup() {
-        baseURI = ConfigurationReader.getProperty("calendarific.uri");
-    }
+            @BeforeAll
+            public static void setup() {
+                baseURI = ConfigurationReader.getProperty("calendarific.uri");
+            }
 
 
 
-    @Test
-    @DisplayName("user verifies that total number of holidays in United Kingdom is equals to 95")
-    public void TC1(){
-        Response response =
-                given().accept(ContentType.JSON).
-                        queryParam("api_key","5b366e8be6fa6e1aeb9b5636c093690a94b09c85")
-                        .when().get("/countries").prettyPeek();
-        response.then().assertThat().statusCode(200);
-        //Gpath - it's like Xpath, stands for searching values in JSON file.
-        //GPath: response.countries.find {it.country_name == 'United Kingdom'}.total_holidays
-        //find - method to find some parameter
-        //{it.parameter_name == value} find JSON object that is matching criteria
-        //.parameter_that_you_want = return this parameter after filtering
-        int numberHolidays = response.jsonPath().get("response.countries.find {it.country_name == 'United Kingdom'}.total_holidays");
+            @Test
+            @DisplayName("user verifies that total number of holidays in United Kingdom is equals to 95")
+            public void TC1(){
+                Response response=
+                        given().accept(ContentType.JSON).
+                                queryParam("api_key","5b366e8be6fa6e1aeb9b5636c093690a94b09c85")
+                                .when().get("/countries").prettyPeek();
+                response.then().assertThat().statusCode(200);
+                //Gpath - it's like Xpath, stands for searching values in JSON file.
+                //GPath: response.countries.find {it.country_name == 'United Kingdom'}.total_holidays
+                //find - method to find some parameter
+                //{it.parameter_name == value} find JSON object that is matching criteria
+                //.parameter_that_you_want = return this parameter after filtering
+                int numberHolidays = response.jsonPath().get("response.countries.find {it.country_name == 'United Kingdom'}.total_holidays");
 
-        //get all countries where number supported_languages is = 4
+                //get all countries where number supported_languages is = 4
 
-        List<String> countries = response.jsonPath().get("response.countries.findAll {it.supported_languages == 4}.country_name");
+                List<String> countries = response.jsonPath().get("response.countries.findAll {it.supported_languages == 4}.country_name");
 
-        System.out.println(countries);
+                System.out.println(countries);
 
-        assertEquals(95,numberHolidays);
-        System.out.println("MY TESTS PASSED SUCCESSFULLY😍😍");
-    }
+                assertEquals(95,numberHolidays);
+                System.out.println("MY TESTS PASSED SUCCESSFULLY😍😍");
+            }
 
-    @Test
-    @DisplayName("user verifies that total number of holidays in United Kingdom is equals to 95")
-    public void TC1_2(){
-        Response response=
-                given().accept(ContentType.JSON).
-                        queryParam("api_key","5b366e8be6fa6e1aeb9b5636c093690a94b09c85")
-                        .queryParam("country","GB")
-                        .queryParam("year",2019)
-                        .when().get("/holidays").prettyPeek();
-        response.then().assertThat().statusCode(200);
-        List<Map<String,?>> holidays=response.jsonPath().get("response.holidays");
-        assertEquals(95,holidays.size());
-        System.out.println("MY TESTS PASSED SUCCESSFULLY😍😍");
-    }
+            @Test
+            @DisplayName("user verifies that total number of holidays in United Kingdom is equals to 95")
+            public void TC1_2(){
+                Response response=
+                        given().accept(ContentType.JSON).
+                                queryParam("api_key","5b366e8be6fa6e1aeb9b5636c093690a94b09c85")
+                                .queryParam("country","GB")
+                                .queryParam("year",2019)
+                                .when().get("/holidays").prettyPeek();
+                response.then().assertThat().statusCode(200);
+                List<Map<String,?>> holidays=response.jsonPath().get("response.holidays");
+                assertEquals(95,holidays.size());
+                System.out.println("MY TESTS PASSED SUCCESSFULLY😍😍");
+            }
 
-}
+        }
